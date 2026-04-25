3c8a4ba1ccbd1b257993c5616f8f0230ca1c86ea
Henrique A. R. <henrique4r.it@gmail.com>
2026-04-25 18:14:11 -0300

    feat: implementa a variação de foto por id de Desenvolvedor

--- src/screens/SobreMim/SobreMim.tsx
+++ src/screens/SobreMim/SobreMim.tsx
@@ -2,15 +2,21 @@ import { View, Text } from "react-native";
 import { desenvolvedores } from "../../data/desenvolvedores";
 import type { SobreMimScreenProps } from "../../navigation/AppNavigator";
 import { styles } from "./SobreMimStyle";
+import { FotoPerfil } from "../../components/FotoPerfil/FotoPerfil";
 
 export function SobreMim({ route }: SobreMimScreenProps){
     const { idDesenvolvedor } = route.params;
-    const desenvolvedor = desenvolvedores[idDesenvolvedor];
+    const desenvolvedor = desenvolvedores.find((dev) => dev.id === idDesenvolvedor);
+
+    if (!desenvolvedor) {
+        throw new Error("Desenvolvedor não localizado");
+    }
 
 
     return(
-        <View style={styles.container}>
-            <Text>Nome do desenvolvedor: {desenvolvedor.nome}</Text>
+        <View style={[styles.container, styles.root]}>
+            <FotoPerfil idDesenvolvedor={idDesenvolvedor} />
+            <Text style={styles.texto}>Nome do desenvolvedor: {desenvolvedor.nome}</Text>
         </View>
     )
 }
--- src/screens/SobreMim/SobreMimStyle.tsx
+++ src/screens/SobreMim/SobreMimStyle.tsx
@@ -1,9 +1,19 @@
 import { StyleSheet } from "react-native";
+import { tema } from "../../Tema";
 
 export const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center",
-	}
-});
+	},
+
+    root: {
+        flex: 1,
+        backgroundColor: tema.background,
+    },
+    texto: {
+        backgroundColor: '#fff'
+    }
+
+})
